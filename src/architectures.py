26e8c783a65319b881acc0c8b4017c179c96caf7
SebastianLoef <sebastian@loef.se>
2023-06-29 16:04:01 +0200

    Changed default number of channels from 3 to 1

--- src/architectures.py
+++ src/architectures.py
@@ -55,7 +55,7 @@ class ConvNeXt(nn.Module):
         layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
         head_init_scale (float): Init scaling value for classifier weights and biases. Default: 1.
     """
-    def __init__(self, in_chans=3, num_classes=1000, 
+    def __init__(self, in_chans=1, num_classes=1000, 
                  depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], drop_path_rate=0., 
                  layer_scale_init_value=1e-6, head_init_scale=1.,
                  ):
